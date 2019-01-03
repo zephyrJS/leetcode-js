e7031efca378a1902b208c5b9328936e0cd7c654
zephyr <zhengzefanyy@gmail.com>
2019-01-03 10:19:25 +0800

    remove duplicates from sorted array

---
+++ 026-Remoe-Duplicates-From-Sorted-Array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    if (nums.length === 0) return 0;
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] === nums[j]) {
+            nums.splice(i, 1);
+            j--;
+        } else {
+            i++;
+        }
+    }
+    return i + 1;
+};
+
+// best practice
+var removeDuplicates = function (nums) {
+    if (nums.length === 0) return 0;
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
