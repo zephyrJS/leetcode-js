90e94422a06b41414f284b9ea712bdb66a4dd526
zephyr <zhengzefanyy@gmail.com>
2019-01-04 16:51:27 +0800

    maxinum subarray

---
+++ 053-Maxinum-Subarray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    const n = nums.length;
+    const dp = []; //dp[i] means the maximum subarray ending with A[i];
+    dp[0] = nums[0];
+    let max = dp[0];
+
+    for(let i = 1; i < n; i++){
+        dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+        max = Math.max(max, dp[i]);
+    }
+
+    return max;
+};
