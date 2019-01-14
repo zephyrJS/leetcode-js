5dcbbc47eff9eb600fa8bb4b378ef45e1a1644d4
zephyr <zhengzefanyy@gmail.com>
2019-01-14 09:55:00 +0800

    less variable

--- 112-Best-Time-To-Buy-And-Sell-Stock2.js
+++ 112-Best-Time-To-Buy-And-Sell-Stock2.js
@@ -18,3 +18,16 @@ var maxProfit = function(prices) {
     }
     return maxprofit
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxprofit = 0;
+    for(let i=1; i<prices.length; i++) {
+        if(prices[i] > prices[i-1])
+            maxprofit += prices[i] - prices[i-1];
+    }
+    return maxprofit
+};
