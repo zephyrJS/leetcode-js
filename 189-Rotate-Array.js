029f5271dbefed6c542bfa1fcc56dd032e340e33
zephyr <zhengzefanyy@gmail.com>
2019-01-18 13:52:05 +0800

    rotate array

--- 189-Rotate-Array.js
+++ 189-Rotate-Array.js
@@ -13,3 +13,26 @@ var rotate = function(nums, k) {
 	}
 	return nums
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k %= nums.length
+	nums = reverse(nums, 0, nums.length-1)
+	nums = reverse(nums, 0, k-1)
+	nums = reverse(nums, k, nums.length - 1)	
+    return nums
+};
+function reverse(nums, start, end) {
+	while(start < end) {
+		let temp = nums[end]
+		nums[end] = nums[start]
+		nums[start] = temp
+		start++
+		end--		
+	}	
+    return nums	
+}
