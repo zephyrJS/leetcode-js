d0f4a2fa5b5c0b3095d2110889992de643db7f1e
zephyr <zhengzefanyy@gmail.com>
2019-01-21 15:46:43 +0800

    remove zeroes

--- 283-Move-Zeroes.js
+++ 283-Move-Zeroes.js
@@ -10,3 +10,39 @@ var moveZeroes = function(nums) {
         }
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+	let pos = 0;
+	for(let i=0; i<nums.length; i++) {
+		if(nums[i] !== 0) {
+			nums[pos] = nums[i]
+			pos++
+		}
+	}
+	for(;pos<nums.length;pos++) {
+		nums[pos] = 0
+	}
+}
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+	let x = y = 0;
+    while(y < nums.length) {
+        if(nums[y] == 0) {
+            y++
+        } else {
+            temp = nums[y]
+            nums[y] = nums[x]
+            nums[x] = temp
+            x++
+            y++            
+        }        
+    }
+}
