4670cccf302b1612e8930b95e2dfdb651b5611f2
zephyr <zhengzefanyy@gmail.com>
2019-02-01 23:40:21 +0800

    valid parenthess

---
+++ 020-Valid-Parenthess.js
@@ -0,0 +1,45 @@
+class Stack {
+    constructor() {
+      this. stack = []
+    }
+    push( item ) {
+      this. stack .push( item )
+    }
+    pop() {
+      this. stack .pop()
+    }
+    peek() {
+      return this. stack [this.getCount() - 1]
+    }
+    getCount() {
+      return this. stack . length
+    }
+    isEmpty() {
+      return this.getCount() === 0
+    }
+  }
+  /**
+   * @param {string} s
+   * @return {boolean}
+   */
+  var isValid = function(s) {
+    let map = {
+      '(': -1,
+      ')': 1,
+      '[': -2,
+      ']': 2,
+      '{': -3,
+      '}': 3
+    }
+    let stack = []
+    for (let i = 0; i < s . length ; i ++) {
+      if ( map [ s [ i ]] < 0) {
+        stack .push( s [ i ])
+      } else {
+        let last = stack .pop()
+        if ( map [ last ] + map [ s [ i ]] != 0) return false
+      }
+    }
+    if ( stack . length > 0) return false
+    return true    
+  };
