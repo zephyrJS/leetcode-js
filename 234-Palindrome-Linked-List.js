63c01e3391817a3cf801c74009a387f7c5328439
zephyr <zhengzefanyy@gmail.com>
2019-03-27 13:26:03 +0800

    better solution to  palindrome link list

--- 234-Palindrome-Linked-List.js
+++ 234-Palindrome-Linked-List.js
@@ -23,3 +23,47 @@ var isPalindrome = function(head) {
     }
     return true
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let fast = slow = head
+    while(fast !== null && fast.next !== null) {
+        fast = fast.next.next
+        slow = slow.next
+    }
+    if(fast !== null) {
+        slow = slow.next
+    }
+    slow = reverse(slow)
+    fast = head
+    
+    while(slow !== null) {
+        if(slow.val !== fast.val) {
+            return false
+        }
+        fast = fast.next
+        slow = slow.next
+    }
+    return true
+};
+
+function reverse(head) {
+    let prev = null
+    while(head !== null) {
+        next = head.next
+        head.next = prev
+        prev = head
+        head = next
+    }
+    return prev
+}
